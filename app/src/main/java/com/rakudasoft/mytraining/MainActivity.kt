48dfe36b19ad40ca213b9931c5a7561ec23f1cf2
豊 江川 <yutaka.egawa@gmail.com>
2021-07-03 11:22:03 +0900

    firebase dependencies

--- app/src/main/java/com/rakudasoft/mytraining/MainActivity.kt
+++ app/src/main/java/com/rakudasoft/mytraining/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.rakudasoft.mytraining
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rakudasoft.mytraining.ui.login.LoginActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
     }
 }
